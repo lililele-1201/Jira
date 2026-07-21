c4eae779fba2d53ad53c1f0e14b7b7993ef24511
leledawang <2084355159@qq.com>
2026-07-21 21:04:47 +0800

    任务组页面开发和完成popover(收藏时及时更新)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import "./utils/wdyr";
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,12 +7,10 @@ import 'antd/dist/antd.less'
 import { AppProviders } from 'context';
 
 loadServer(() => ReactDOM.render(
-  <React.StrictMode>
-    <AppProviders>
-      <DevTools/>
-      <App />
-    </AppProviders>
-  </React.StrictMode>,
+  <AppProviders>
+    <DevTools/>
+    <App />
+  </AppProviders>,
   document.getElementById('root') as HTMLElement
 )
 )
